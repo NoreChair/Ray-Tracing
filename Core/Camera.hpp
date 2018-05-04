a79fbb36a1a14b1a7702da5c965eff17d0367541
yl2 <yl2@meitu.com>
2018-05-04 17:10:42 +0800

    增加摄像机位置

--- Core/Camera.hpp
+++ Core/Camera.hpp
@@ -4,10 +4,24 @@ class Camera
 {
 public:
 	Camera() {}
+	Camera(glm::vec3 eye,glm::vec3 lookat,glm::vec3 up,float fov,float aspect) {
+		glm::vec3 u, v, w;
+		float theta = glm::radians(fov);
+		float half_h = glm::tan(theta / 2.0f);
+		float half_w = aspect * half_h;
+		_origin = eye;
+		w = glm::normalize(eye - lookat);
+		u = glm::normalize(glm::cross(up, w));
+		v = glm::cross(w, u);
+		_bottom_left_corner = _origin - half_w * u - half_h * v - w;
+		_horizontal = 2.0f*half_h*u;
+		_vertical = 2.0f*half_w*v;
+
+	}
 	~Camera() {}
 
 
-	Ray Get_ray(float u, float v) { return Ray(_origin, _bottom_left_corner + u * _horizontal + v * _vertical); }
+	Ray Get_ray(float u, float v) { return Ray(_origin, _bottom_left_corner + u * _horizontal + v * _vertical-_origin); }
 
 
 	/// 屏幕左下坐标点
